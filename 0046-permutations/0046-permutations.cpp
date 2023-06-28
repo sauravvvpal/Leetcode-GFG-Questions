fd1b9940b9071f75d1172032a0c26995b90ac793
sauravvvpal <100898011+sauravvvpal@users.noreply.github.com>
2023-06-28 12:59:09 +0530

    Time: 14 ms (5.71%), Space: 9.2 MB (5.01%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void permutations(int ind,vector<int>arr,int n,vector<vector<int>>&ans){
+      if(ind==n){
+          ans.push_back(arr);
+          return ;
+      }
+      unordered_set<int>m;
+      
+      for(int i=ind;i<n;i++){
+          if(m.find(arr[i])!=m.end()) continue;
+          m.insert(arr[i]);
+          swap(arr[ind],arr[i]);
+          permutations(ind+1,arr,n,ans);
+          swap(arr[ind],arr[i]);
+      }
+  }
+    vector<vector<int>> permute(vector<int>& nums) {
+         vector<vector<int>>ans;
+        int n=nums.size();
+        permutations(0,nums,n,ans);
+        sort(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
