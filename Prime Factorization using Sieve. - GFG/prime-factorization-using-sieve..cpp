494ba19e0b4bed5385e2daff08a1dceb0cf0d5b4
sauravvvpal <100898011+sauravvvpal@users.noreply.github.com>
2023-07-06 13:17:43 +0530

    Added solution - LeetHub

---
+++ Prime Factorization using Sieve. - GFG/prime-factorization-using-sieve..cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void sieve() {}
+
+    vector<int> findPrimeFactors(int N) {
+
+        // Write your code here
+        vector<int> ans ;
+        int number = 2 ;
+        for( ; number <= N ; number++)
+        {
+            while(N%number == 0)
+            {
+                ans.push_back(number) ;
+                N /= number ;
+            }
+        }
+        return ans ;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        int n;
+        cin >> n;
+
+        Solution obj;
+        obj.sieve();
+        vector<int> vec = obj.findPrimeFactors(n);
+        for (int i = 0; i < vec.size(); i++) {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
