b1ce6ab7fcadecfde028b42a42abb03042a2c516
sauravvvpal <100898011+sauravvvpal@users.noreply.github.com>
2023-07-31 19:29:16 +0530

    Time: 35 ms (50.58%), Space: 18.6 MB (36.57%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -4,16 +4,13 @@ public:
         int n = coins.size();
         vector<vector<int>>dp(n+1,vector<int>(amount+1));
         
-        for(int j=1;j<=amount;j++){
-            dp[0][j]=0;
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
         
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]>j) dp[i][j]=dp[i-1][j];
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(j==0) dp[i][j]=1;
+                else if(i==0) dp[i][j]=0;
+                else if(coins[i-1]>j) dp[i][j]=dp[i-1][j];
                 else
                     dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
             }
